ddf536466a50017a14785f2987ce55a4dec9afcc
Abdullah Al Faiyaz <abdullah-al.faiyaz@stud.uni-bamberg.de>
2025-08-31 23:45:31 +0200

    question bug fixed

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -9,8 +9,15 @@ const questions = document.querySelectorAll(".question");
 // console.log(questionBtns);
 
 questions.forEach((question) => {
-  question.addEventListener("click", () => {
-    console.log(question);
+  const btn = question.querySelector(".question-btn");
+  //   console.log(btn);
+  btn.addEventListener("click", () => {
+    questions.forEach((item) => {
+      //   console.log(item);
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
     question.classList.toggle("show-text");
   });
 });
