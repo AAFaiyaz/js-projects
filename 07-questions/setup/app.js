9418c0a65e1d9137011e05d49130ab13ad560acb
Abdullah Al Faiyaz <abdullah-al.faiyaz@stud.uni-bamberg.de>
2025-08-31 23:31:21 +0200

    question project done

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,17 @@
 //using selectors inside the element
+
+const questions = document.querySelectorAll(".question");
+
+// const questionBtns = document.querySelectorAll(".question-btn");
+
+// console.log(questions);
+
+// console.log(questionBtns);
+
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    console.log(question);
+    question.classList.toggle("show-text");
+  });
+});
 // traversing the dom
